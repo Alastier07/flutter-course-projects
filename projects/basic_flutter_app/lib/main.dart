6fff9e4cdded45fb62b1e76f01098363e6cd6348
Alastier <calastier07@gmail.com>
2022-02-03 13:42:22 +0800

    adding column in the body and input text and button widgets

--- projects/basic_flutter_app/lib/main.dart
+++ projects/basic_flutter_app/lib/main.dart
@@ -4,6 +4,10 @@ import 'package:flutter/material.dart';
 //  runApp(MyApp());
 //}
 
+void answerButton() {
+  print('Answer Chosen!');
+}
+
 void main() => runApp(MyApp()); // => make function in one expression
 
 class MyApp extends StatelessWidget {
@@ -14,7 +18,23 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Basic App'),
         ),
-        body: Text("Hi welcome to my app!"),
+        body: Column(
+          children: [
+            Text('The questions'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerButton, //call the name function not the result
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerButton,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerButton,
+            ),
+          ],
+        ),
       ), // Create basic design to app
     );
   }
