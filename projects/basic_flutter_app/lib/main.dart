e61cd2878b62b43d74291b8bc6bfdca16b359667
Alastier <calastier07@gmail.com>
2022-02-04 12:22:41 +0800

    split widget for quiz and results

--- projects/basic_flutter_app/lib/main.dart
+++ projects/basic_flutter_app/lib/main.dart
@@ -2,8 +2,10 @@
 import 'package:flutter/material.dart';
 
 //files
-import './questions.dart';
-import './answer.dart';
+//import './questions.dart';
+//import './answer.dart';
+import './quiz.dart';
+import './results.dart';
 //void main() {
 //  runApp(MyApp());
 //}
@@ -19,6 +21,21 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What is your favorite color?',
+      'answers': ['Blue', 'Red', 'Green', 'Yellow'],
+    },
+    {
+      'questionText': 'What is your favorite animal?',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'What is your favorite filipino food?',
+      'answers': ['Adobo', 'Sinigang', 'Letchon', 'Nilaga'],
+    },
+  ];
+
   var _questionIndex = 0;
 
   void _answerButton() {
@@ -30,36 +47,18 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': 'What is your favorite color?',
-        'answers': ['Blue', 'Red', 'Green', 'Yellow'],
-      },
-      {
-        'questionText': 'What is your favorite animal?',
-        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
-      },
-      {
-        'questionText': 'What is your favorite filipino food?',
-        'answers': ['Adobo', 'Sinigang', 'Letchon', 'Nilaga'],
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Basic App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex]['questionText'] as String,
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerButton, answer);
-            }).toList() // ... spread operator move 1 list to another
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerButton: _answerButton,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Results(),
       ), // Create basic design to app
     );
   }
