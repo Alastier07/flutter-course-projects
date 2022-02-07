980f66cb43be8cdf063c5890b0eea4e42a3095b2
Alastier <calastier07@gmail.com>
2022-02-07 11:37:11 +0800

    add style to the container

--- projects/personal_expenses_app/lib/main.dart
+++ projects/personal_expenses_app/lib/main.dart
@@ -59,6 +59,17 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: Row(
                     children: <Widget>[
                       Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
                         child: Text(
                           tx.amount.toString(),
                         ),
