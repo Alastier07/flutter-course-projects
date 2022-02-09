9a815a133ff34907d846e56b98801d98b3c9e9d5
Alastier <calastier07@gmail.com>
2022-02-09 16:44:26 +0800

    add soft keyboard inset by scrolling

--- projects/personal_expenses_app/lib/widgets/new_transaction.dart
+++ projects/personal_expenses_app/lib/widgets/new_transaction.dart
@@ -59,59 +59,66 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            // Input for title
-            TextField(
-              decoration: InputDecoration(labelText: 'Title'),
-              //onChanged: (val) => titleInput = val,
-              controller: _titleController,
-              onSubmitted: (_) => _submitData(),
-            ),
-            // Input for amount
-            TextField(
-              decoration: InputDecoration(labelText: 'Amount'),
-              //onChanged: (val) => amountInput = val,
-              controller: _amountController,
-              keyboardType: TextInputType.number,
-              onSubmitted: (_) => _submitData(),
-            ),
-            // DatePicker Text & Button
-            Container(
-              height: 70,
-              child: Row(children: <Widget>[
-                Expanded(
-                  child: Text(
-                    _selectedDate == null
-                        ? 'No Date Chosen!'
-                        : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
-                  ),
-                ),
-                FlatButton(
-                  textColor: Theme.of(context).primaryColor,
-                  child: Text(
-                    'Choose Date',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 10,
+            right: 10,
+            left: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              // Input for title
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+                //onChanged: (val) => titleInput = val,
+                controller: _titleController,
+                onSubmitted: (_) => _submitData(),
+              ),
+              // Input for amount
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount'),
+                //onChanged: (val) => amountInput = val,
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => _submitData(),
+              ),
+              // DatePicker Text & Button
+              Container(
+                height: 70,
+                child: Row(children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      _selectedDate == null
+                          ? 'No Date Chosen!'
+                          : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                     ),
                   ),
-                  onPressed: _presentDatePicker,
-                ),
-              ]),
-            ),
-            // Submit Button for adding transaction
-            RaisedButton(
-              onPressed: _submitData,
-              child: Text('Add Transaction'),
-              textColor: Theme.of(context).textTheme.button.color,
-              color: Theme.of(context).primaryColor,
-            )
-          ],
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onPressed: _presentDatePicker,
+                  ),
+                ]),
+              ),
+              // Submit Button for adding transaction
+              RaisedButton(
+                onPressed: _submitData,
+                child: Text('Add Transaction'),
+                textColor: Theme.of(context).textTheme.button.color,
+                color: Theme.of(context).primaryColor,
+              )
+            ],
+          ),
         ),
       ),
     );
