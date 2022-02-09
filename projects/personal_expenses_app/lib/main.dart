b1fab8a45d63de5b3ac71a4484497303bb7103dd
Alastier <calastier07@gmail.com>
2022-02-09 13:56:56 +0800

    adding app controlling orientation to portraint only

--- projects/personal_expenses_app/lib/main.dart
+++ projects/personal_expenses_app/lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import './widgets/chart.dart';
 import './widgets/new_transaction.dart';
 import './widgets/transaction_list.dart';
 import './models/transaction.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]); // Control app orientation to portrait only
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
