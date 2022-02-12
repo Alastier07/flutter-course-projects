0d938bd8dbaca076202622d717327a4a804bda29
Alastier <calastier07@gmail.com>
2022-02-12 19:44:22 +0800

    add theme and style

--- projects/meals_app/lib/categories_screen.dart
+++ projects/meals_app/lib/categories_screen.dart
@@ -7,16 +7,17 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ArMeals'),
+        title: const Text('ArMeals'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(15),
         children: DUMMY_CATEGORIES
             .map((catData) => CategoryItem(
                   catData.title,
                   catData.color,
                 ))
             .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
