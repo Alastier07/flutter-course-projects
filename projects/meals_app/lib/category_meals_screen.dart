f1bea12adccec7c02bfa6b0f079dfd1f9006c775
Alastier <calastier07@gmail.com>
2022-02-13 01:00:41 +0800

    add navigating to a new page with passing data via constructor

--- projects/meals_app/lib/category_meals_screen.dart
+++ projects/meals_app/lib/category_meals_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  final String categyId;
+  final String categoryTitle;
+
+  CategoryMealsScreen(this.categyId, this.categoryTitle);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'The Recipes',
+          categoryTitle,
         ),
       ),
       body: Center(
