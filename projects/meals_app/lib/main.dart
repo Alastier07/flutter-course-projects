1d351c84661dbdb7154e5f47185c4b615a6896a2
Alastier <calastier07@gmail.com>
2022-02-14 11:26:43 +0800

    adding tab bar to the appbar and create favorites screen

--- projects/meals_app/lib/main.dart
+++ projects/meals_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './screens/tabs_screen.dart';
 import './screens/meal_detail_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/categories_screen.dart';
@@ -39,7 +40,7 @@ class _MyAppState extends State<MyApp> {
       //home: CategoriesScreen(),
       initialRoute: '/',
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MeaLDetailScreen.routeName: (ctx) => MeaLDetailScreen(),
       },
---
+++ projects/meals_app/lib/screens/tabs_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import '../screens/favorites_screen.dart';
+import '../screens/categories_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      //initialIndex: 1, //Set which default tab bar to be shown
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Meals'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(
+                  Icons.category,
+                ),
+                text: 'Categories',
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.star,
+                ),
+                text: 'Favorites',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            CategoriesScreen(),
+            FavoritesScreen(),
+          ],
+        ),
+      ),
+    );
+  }
+}
