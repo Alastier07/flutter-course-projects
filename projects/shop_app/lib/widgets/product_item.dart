5821cb3359829b0441cc17d9b9c09b7bd599a06c
Alastier <calastier07@gmail.com>
2022-02-15 15:12:52 +0800

    use pushName navigation

--- projects/shop_app/lib/widgets/product_item.dart
+++ projects/shop_app/lib/widgets/product_item.dart
@@ -14,10 +14,9 @@ class ProductItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(10),
       child: GestureDetector(
         onTap: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (ctx) => ProductDetailScreen(title),
-            ),
+          Navigator.of(context).pushNamed(
+            ProductDetailScreen.routeName,
+            arguments: id,
           );
         },
         child: GridTile(
