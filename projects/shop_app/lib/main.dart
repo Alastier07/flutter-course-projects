c8d561079c8b162403c02f91df436adbbe244966
Alastier <calastier07@gmail.com>
2022-02-16 13:59:49 +0800

    connecting to cart provider

--- projects/shop_app/lib/main.dart
+++ projects/shop_app/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
+import './providers/cart.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,6 +15,9 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(
           create: (ctx) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Cart(),
         ),
       ],
       child: MaterialApp(
