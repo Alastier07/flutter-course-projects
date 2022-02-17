9a0dc0981e49b5fa2a2fc22088914cbd1e5d0b58
Alastier <calastier07@gmail.com>
2022-02-17 18:37:32 +0800

    add validating user input in title

--- projects/shop_app/lib/screens/edit_product_screen.dart
+++ projects/shop_app/lib/screens/edit_product_screen.dart
@@ -47,6 +47,10 @@ class _EditProductScreenState extends State<EditProductScreen> {
   }
 
   void _saveForm() {
+    final isValid = _form.currentState.validate();
+    if (!isValid) {
+      return;
+    }
     _form.currentState.save();
   }
 
@@ -73,6 +77,13 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please provide a value.';
+                  } else {
+                    return null;
+                  }
                 },
                 onSaved: (value) {
                   _editedProduct = Product(
