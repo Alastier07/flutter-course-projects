db46a27def76c829ef5d42cd733f1a388b83d200
Alastier <calastier07@gmail.com>
2022-02-17 11:32:59 +0800

    add showing alert dialogs

--- projects/shop_app/lib/widgets/cart_item.dart
+++ projects/shop_app/lib/widgets/cart_item.dart
@@ -33,6 +33,31 @@ class CartItem extends StatelessWidget {
         ),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text(
+              'Do you want to remove the item fron the cart?',
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Yes'),
+                onPressed: () {
+                  Navigator.of(ctx).pop(true);
+                },
+              ),
+              FlatButton(
+                child: Text('No'),
+                onPressed: () {
+                  Navigator.of(ctx).pop(false);
+                },
+              ),
+            ],
+          ),
+        );
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
