e4b8e0ad18f0336949c0cfd5e3f2190d66f052cc
Alastier <calastier07@gmail.com>
2022-02-18 01:21:26 +0800

    add saving a new produt function

--- projects/shop_app/lib/screens/edit_product_screen.dart
+++ projects/shop_app/lib/screens/edit_product_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
 import '../providers/product.dart';
+import '../providers/products.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product-screen';
@@ -60,6 +63,8 @@ class _EditProductScreenState extends State<EditProductScreen> {
       return;
     }
     _form.currentState.save();
+    Provider.of<Products>(context, listen: false).addProduct(_editedProduct);
+    Navigator.of(context).pop();
   }
 
   @override
