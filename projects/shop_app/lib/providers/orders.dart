ab283b7cd88329d42b3d642f2583a0f12419e7f8
Alastier <calastier07@gmail.com>
2022-02-21 14:09:28 +0800

    Adding the Token to All Requests

--- projects/shop_app/lib/providers/orders.dart
+++ projects/shop_app/lib/providers/orders.dart
@@ -21,6 +21,9 @@ class OrderItem {
 
 class Orders with ChangeNotifier {
   List<OrderItem> _orders = [];
+  final String authToken;
+
+  Orders(this.authToken, this._orders);
 
   List<OrderItem> get orders {
     return [..._orders];
@@ -28,7 +31,7 @@ class Orders with ChangeNotifier {
 
   Future<void> fetchAndSetOrders() async {
     final url = Uri.parse(
-        'https://flutter-training-a2482-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json');
+        'https://flutter-training-a2482-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json?auth=${authToken}');
     final response = await http.get(url);
     // print(json.decode(response.body));
     final List<OrderItem> loadedOrders = [];
@@ -61,7 +64,7 @@ class Orders with ChangeNotifier {
 
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
     final url = Uri.parse(
-        'https://flutter-training-a2482-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json');
+        'https://flutter-training-a2482-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json?auth=${authToken}');
     final timestamp = DateTime.now();
     final response = await http.post(url,
         body: json.encode({
