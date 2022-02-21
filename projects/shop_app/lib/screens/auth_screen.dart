0fcd5304eff6ff76512d949b652667dbd4489e74
Alastier <calastier07@gmail.com>
2022-02-21 13:27:50 +0800

    Managing the Auth Token Locally in the app

--- projects/shop_app/lib/screens/auth_screen.dart
+++ projects/shop_app/lib/screens/auth_screen.dart
@@ -135,12 +135,16 @@ class _AuthCardState extends State<AuthCard> {
     try {
       if (_authMode == AuthMode.Login) {
         // Log user in
-        await Provider.of<Auth>(context, listen: false)
-            .login(_authData['email'], _authData['password']);
+        await Provider.of<Auth>(context, listen: false).login(
+          _authData['email'],
+          _authData['password'],
+        );
       } else {
         // Sign user up
-        await Provider.of<Auth>(context, listen: false)
-            .singup(_authData['email'], _authData['password']);
+        await Provider.of<Auth>(context, listen: false).singup(
+          _authData['email'],
+          _authData['password'],
+        );
       }
     } on HttpException catch (error) {
       var errorMessage = 'Authentication failed';
