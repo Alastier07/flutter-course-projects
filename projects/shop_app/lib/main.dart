99f74669de62b502a4f7062b44f44eeda07c650d
Alastier <calastier07@gmail.com>
2022-02-22 13:10:56 +0800

    Implementing Custom Route Transitions

--- projects/shop_app/lib/main.dart
+++ projects/shop_app/lib/main.dart
@@ -10,6 +10,8 @@ import './screens/edit_product_screen.dart';
 import './screens/auth_screen.dart';
 import './screens/splash_screen.dart';
 
+import './helpers/custom_route.dart';
+
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
@@ -50,6 +52,12 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.purple,
             accentColor: Colors.deepOrange,
             fontFamily: 'Lato',
+            pageTransitionsTheme: PageTransitionsTheme(
+              builders: {
+                TargetPlatform.android: CustomPageTransitionBuilder(),
+                TargetPlatform.iOS: CustomPageTransitionBuilder(),
+              },
+            ),
           ),
           home: auth.isAuth
               ? ProductsOverviewScreen()
